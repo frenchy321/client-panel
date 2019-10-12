1039382a063653e559df16a732471c82153ec59f
Andy <andy.french@hotmail.co.uk>
2019-10-12 12:08:39 +0100

    Copy and pasted 'add-client' html into 'edit-client' html, and imported file into 'edit-client.ts'

--- src/app/components/edit-client/edit-client.component.ts
+++ src/app/components/edit-client/edit-client.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../../services/client.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { Client } from '../../models/Client';
 
 @Component({
   selector: 'app-edit-client',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-client.component.css']
 })
 export class EditClientComponent implements OnInit {
+  id: string;
+  client: Client = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    balance: 0
+  };
 
-  constructor() { }
+  disableBalanceOnEdit: boolean = true;
+
+  constructor(
+    private clientService: ClientService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private flashMessages: FlashMessagesService
+  ) {}
 
   ngOnInit() {
+    // get id from url
+    this.id = this.route.snapshot.params['id'];
+    // get client
+    this.clientService
+      .getClient(this.id)
+      .subscribe(client => (this.client = client));
   }
 
+  onSubmit({ value, valid }: { value: Client; valid: boolean }) {}
 }
