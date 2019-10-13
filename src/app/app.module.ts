700ac83046bcde055fcd268eb17aeb50e8dd491c
Andy <andy.french@hotmail.co.uk>
2019-10-14 00:40:37 +0100

    Created 'login' method and imported files.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,8 @@ import { RegisterComponent } from './components/register/register.component';
 import { SettingsComponent } from './components/settings/settings.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ClientService } from './services/client.service';
+import { AuthService } from './services/auth.service';
 
 @NgModule({
   declarations: [
@@ -45,7 +47,7 @@ import { AppRoutingModule } from './app-routing.module';
     AngularFirestoreModule,
     AngularFireAuthModule
   ],
-  providers: [],
+  providers: [ClientService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularFire2/auth';
-import { Observable } from 'rxjs/observable';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   constructor(private afAuth: AngularFireAuth) {}
+
+  login(email: string, password: string) {
+    return new Promise((resolve, reject) => {
+      this.afAuth.auth
+        .signInWithEmailAndPassword(email, password)
+        .then(userData => resolve(userData), err => reject(err));
+    });
+  }
 }
