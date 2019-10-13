99ae0280a3aec7e6efb0b4fbf5d8039fb7645a5d
Andy <andy.french@hotmail.co.uk>
2019-10-14 00:12:31 +0100

    Imported files into auth.service.ts and injected into constructor. Deleted spec file.

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularFire2/auth';
+import { Observable } from 'rxjs/observable';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) {}
 }
