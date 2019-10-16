f7c4cad8365679afc0d1a292b72d2a376156ebe9
Andy <andy.french@hotmail.co.uk>
2019-10-16 11:47:37 +0100

    Created 'settings' interface and properties, and 'getSettings' method.

--- src/app/services/settings.service.ts
+++ src/app/services/settings.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Settings } from '../models/settings';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SettingsService {
+  settings: Settings = {
+    allowRegistration: true,
+    disableBalanceOnAdd: false,
+    disableBalanceOnEdit: false
+  };
 
-  constructor() { }
+  constructor() {}
+
+  getSettings(): Settings {
+    return this.settings;
+  }
 }
